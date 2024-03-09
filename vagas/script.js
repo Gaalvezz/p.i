845a10f8ce72bf421a9552df0b8874a92eb83427
Gaalvezz <gabriel.b.alves2@gmail.com>
2024-03-09 00:38:32 -0300

    09ATT

---
+++ vagas/script.js
@@ -0,0 +1,52 @@
+
+function desistir() {
+
+
+    let desistir = document.querySelector('.desistir')
+
+    desistir = confirm("Deseja Desistir ?");
+    
+    if (desistir == true ) {
+        alert("Você deseja continuar na vaga")
+    }
+    
+
+}
+
+
+
+// const btn = document.querySelector('.button-desistir')
+
+// const btn1 = document.querySelector('.button-desistir')
+
+// //fazendo evento de clique
+// btn.addEventListener('click', abrirModal)
+// btn1.addEventListener('click', abrirModal)
+
+
+// function abrirJanela(){
+//     const modal = document.querySelector('.janela')
+//     const modalAtual = modal.style.display
+
+//     if(modalAtual == 'block'){
+//         modal.style.display = 'none'
+//     }else{
+//         modal.style.display = 'block'
+//     }
+// }
+
+
+
+
+// function abrirJanela(){
+//     let janela = document.querySelector('.button-desistir')
+
+//     janela.computedStyleMap.display = "block"
+// }
+
+// function fechaJanela(){
+//     let janela = document.querySelector ('.button-desistir')
+
+//     janela.computedStyleMap.display = 'none'
+// }
+
